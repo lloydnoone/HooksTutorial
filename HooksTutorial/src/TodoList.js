95c781dd367a9122cdeb07e251889765c06e41b1
lloydnoone <lloydnoone92@gmail.com>
2020-01-10 14:15:24 +0000

    finish hooks todo

--- HooksTutorial/src/TodoList.js
+++ HooksTutorial/src/TodoList.js
@@ -7,26 +7,26 @@ import Divider from '@material-ui/core/Divider'
 import Todo from './Todo'
 
 function TodoList({ todos, removeTodo, toggleTodo, editTodo }) {
-  return (
-    <Paper>
-      <List>
-        {todos.map((todo, i) => (
-          <>
-            <Todo
-              id={todo.id}
-              task={todo.task}
-              key={todo.id}
-              completed={todo.completed}
-              removeTodo={removeTodo}
-              toggleTodo={toggleTodo}
-              editTodo={editTodo}
-            />
-            {i < todos.length - 1 && <Divider/>}
-          </>
-        ))}
-      </List>
-    </Paper>
-  )
+  if (todos.length)
+    return (
+      <Paper>
+        <List>
+          {todos.map((todo, i) => (
+            <>
+              <Todo
+                {...todo}
+                key={todo.id}
+                removeTodo={removeTodo}
+                toggleTodo={toggleTodo}
+                editTodo={editTodo}
+              />
+              {i < todos.length - 1 && <Divider/>}
+            </>
+          ))}
+        </List>
+      </Paper>
+    )
+  return null
 }
 
 export default TodoList
--- HooksTutorial/src/app.js
+++ HooksTutorial/src/app.js
@@ -1,14 +1,10 @@
-import React, { Component } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
 
 import TodoApp from './TodoApp'
 
-class App extends Component {
-  render() {
-    return (
-      <TodoApp/>
-    )
-  }
+function App() {
+  return <TodoApp/>
 }
 
 ReactDOM.render(
