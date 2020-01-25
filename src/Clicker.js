d4c81e473338bb896459076505dc7ac417c96c50
lloydnoone <lloydnoone92@gmail.com>
2020-01-25 20:02:12 +0000

    split contexts

---
+++ src/Clicker.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react'
+function Clicker() {
+  const [count, setCount] = useState(0)
+  useEffect(() => {
+    document.title = count
+  })
+  return (
+    <button onClick={() => setCount(count + 1)}>Click Me {count}</button>
+  )
+}
+
+export default Clicker
